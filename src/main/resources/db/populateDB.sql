6e2f9a8c5066298f8666e7662441980f6ebc19e6
Kosshitikhin <kosshitikhin@gmail.com>
2021-02-23 01:43:38 +0300

    HW3 init and populate DB with a table 'meal'

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,3 +1,4 @@
+DELETE FROM meals;
 DELETE FROM user_roles;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
@@ -9,3 +10,7 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (meal_id, user_id, date_time, description, calories)
+VALUES (1, 100000, '2020-12-20 12:00:00' , 'Обед', 550),
+       (2, 100001,  '2020-12-21 09:00:00', 'Завтрак', 1000);
