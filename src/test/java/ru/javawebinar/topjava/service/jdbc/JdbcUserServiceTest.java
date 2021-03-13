31f938962fa2c56095a6511625c544cea9816db9
Kosshitikhin <kosshitikhin@gmail.com>
2021-03-14 02:30:32 +0300

    HW5

--- src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcUserServiceTest.java
@@ -1,4 +1,10 @@
 package ru.javawebinar.topjava.service.jdbc;
 
-public class JdbcUserServiceTest {
+import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.service.AbstractUserServiceTest;
+
+import static ru.javawebinar.topjava.Profiles.JDBC;
+
+@ActiveProfiles(JDBC)
+public class JdbcUserServiceTest extends AbstractUserServiceTest {
 }
