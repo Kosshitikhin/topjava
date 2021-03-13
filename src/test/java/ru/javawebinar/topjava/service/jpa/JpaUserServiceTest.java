2a8a331cbf5c436685d15f9917a885adfdade31d
Kosshitikhin <kosshitikhin@gmail.com>
2021-03-14 02:32:30 +0300

    HW5

--- src/test/java/ru/javawebinar/topjava/service/jpa/JpaUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/jpa/JpaUserServiceTest.java
@@ -1,4 +1,10 @@
 package ru.javawebinar.topjava.service.jpa;
 
-public class JpaUserServiceTest {
+import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.service.AbstractUserServiceTest;
+
+import static ru.javawebinar.topjava.Profiles.JPA;
+
+@ActiveProfiles(JPA)
+public class JpaUserServiceTest extends AbstractUserServiceTest {
 }
